1f8f8633394e4122e1cbcfea99822ab9bcc9ce76
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-04-09 20:37:32 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { createSignal, type Component, createEffect } from "solid-js";
+import {
+  createSignal,
+  type Component,
+  createEffect,
+  Show,
+  For,
+} from "solid-js";
 
 import styles from "./App.module.css";
 import {
@@ -20,6 +26,7 @@ import {
   Col,
   InputGroup,
   FormControl,
+  Accordion,
 } from "solid-bootstrap";
 import { getHexPubkey, getHexSeckey } from "./function";
 
@@ -37,7 +44,8 @@ const App: Component = () => {
     null
   );
   const [newEvent, setNewEvent] = createSignal<NostrEvent | null>(null);
-
+  // 処理中状態の管理
+  const [processing, setProcessing] = createSignal(false);
   let relay: Relay;
 
   setEvent({
@@ -64,16 +72,17 @@ const App: Component = () => {
     bot?: boolean;
     lud16?: string;
   }
+  //一般的なmetadataに含まれる項目
   const sampleData: Metadata = {
-    name: "",
-    about: "",
-    picture: "",
-    nip05: "",
-    display_name: "",
-    website: "",
-    banner: "",
+    name: "name",
+    about: "about",
+    picture: "https://example.com/picture.webp",
+    nip05: "name@exapmle.com",
+    display_name: "name",
+    website: "https://example.com",
+    banner: "https://example.com/banner.webp",
     bot: false,
-    lud16: "",
+    lud16: "nameswallet@wallet.com",
   };
   let pubhex: string;
   const dataReset = () => {
@@ -95,10 +104,12 @@ const App: Component = () => {
     }
 
     try {
+      setProcessing(true);
+      pubhex = getHexPubkey(pubkey());
       //relayに接続
       relay = await Relay.connect(relayURL());
       console.log(`connected to ${relay.url}`);
-      pubhex = getHexPubkey(pubkey());
+
       // relay.subscribeをPromiseでラップして実行
       await new Promise<void>((resolve, reject) => {
         const sub = relay.subscribe(
@@ -132,6 +143,7 @@ const App: Component = () => {
       setMessage("取得できませんでした");
       setShow(true);
     }
+    setProcessing(false);
   };
 
   // コンテンツの変更を検知して表示を更新
@@ -140,19 +152,22 @@ const App: Component = () => {
   });
 
   const handleAdd = () => {
-    console.log("test");
+    setProcessing(true);
     if (newKey() && newValue()) {
       const updatedContent = { ...content(), [newKey()]: newValue() };
       setContent(updatedContent);
       setNewKey("");
       setNewValue("");
     }
+    setProcessing(false);
   };
 
   const handleDelete = (key: string) => {
+    setProcessing(true);
     const updatedContent = { ...content() };
     delete updatedContent[key];
     setContent(updatedContent);
+    setProcessing(false);
   };
 
   const handleEdit = (key: string | boolean) => {
@@ -170,10 +185,12 @@ const App: Component = () => {
       alert("Install NIP-07 browser extension");
       return;
     }
+    setProcessing(true);
     const pub = await nostr.getPublicKey();
     if (pub) {
       setPubkey(nip19.npubEncode(pub));
     }
+    setProcessing(false);
   };
 
   //dore="nsec"だとnsecによるかきこみ,nullだと拡張機能で
@@ -198,6 +215,7 @@ const App: Component = () => {
         if (key in sampleData && typeof value !== typeof sampleData[key]) {
           setMessage(`不正なデータが含まれています: ${key}`);
           setShow(true);
+
           return;
         }
       }
@@ -212,7 +230,7 @@ const App: Component = () => {
 
       return;
     }
-
+    setProcessing(true);
     let newEvent: NostrEvent = {
       content: JSON.stringify(content()),
       kind: event()?.kind ?? 0,
@@ -228,14 +246,16 @@ const App: Component = () => {
     pubhex = !pubhex ? getHexPubkey(pubkey()) : pubhex;
     console.log(pubhex);
     //イベントチェック
-    if (newEvent.pubkey !== pubhex) {
+    if (pubhex !== "" && newEvent.pubkey !== pubhex) {
       setMessage("check your pubkey");
       setShow(true);
+      setProcessing(false);
       return;
     }
     const check = validateEvent(newEvent);
     if (!check) {
       setMessage("不正なイベントです");
+      setProcessing(false);
       return;
     }
 
@@ -246,135 +266,170 @@ const App: Component = () => {
         ? finalizeEvent(newEvent, getHexSeckey(seckey()))
         : ((await nostr?.signEvent(newEvent)) as NostrEvent);
     setNewEvent(newEvent);
+    setProcessing(false);
   };
 
   const handlePublieshEvent = async () => {
-    const result = await relay.publish(newEvent() as NostrEvent);
-    setMessage("完了しました");
-    setShow(true);
-    relay.close();
-    console.log(result);
+    if (relayURL() == "") {
+      setMessage("check input relayURL");
+      setShow(true);
+      return;
+    }
+    setProcessing(true);
+    try {
+      if (!relay || relay.url !== relayURL() || !relay.connected)
+        relay = await Relay.connect(relayURL());
+      console.log(`connected to ${relay.url}`);
+
+      const result = await relay.publish(newEvent() as NostrEvent);
+      setMessage("完了しました");
+      setShow(true);
+      relay.close();
+      console.log(result);
+    } catch (error) {
+      setMessage("error");
+      setShow(true);
+    }
+    setProcessing(false);
   };
 
   return (
     <>
       <Container fluid="md" class="my-5">
-        <div class={styles.profileHeader}>
-          <h2 class={"fs-2 " + styles.title}>profileを修正するやつ</h2>
-          <a
-            href="https://github.com/TsukemonoGit/nos-profile-arekore"
-            target="_blank"
-            rel="noopener noreferrer"
-            class={styles.githubCol}
-          >
-            Github
-          </a>
-        </div>
-
-        <hr />
-        <h3 class="fs-3">profileを取得する</h3>
-        <Form>
-          <InputGroup class="mb-3">
-            <Button
-              variant="outline-secondary"
-              id="button-addon1"
-              onClick={handleGetPub}
+        <>
+          <div class={styles.profileHeader}>
+            <h3 class="fs-3">profileを修正 / 作成する</h3>{" "}
+            <a
+              href="https://github.com/TsukemonoGit/nos-profile-arekore"
+              target="_blank"
+              rel="noopener noreferrer"
+              class={styles.githubCol}
             >
-              NIP-07,46 <br />
-              から取得
-            </Button>
-            <FormControl
-              aria-label="Example text with button addon"
-              aria-describedby="basic-addon1"
-              type="text"
-              placeholder="npub..."
-              value={pubkey()}
-              onInput={(e) => setPubkey(e.currentTarget.value)}
-            />
-          </InputGroup>
+              Github
+            </a>
+          </div>
+          <hr />
+          <Accordion class="my-4">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>profileを読み込む</Accordion.Header>
+              <Accordion.Body>
+                <Form>
+                  <InputGroup class="mb-3">
+                    <Button
+                      variant="outline-secondary"
+                      id="button-addon1"
+                      onClick={handleGetPub}
+                    >
+                      NIP-07,46 <br />
+                      から取得
+                    </Button>
+                    <FormControl
+                      aria-label="Example text with button addon"
+                      aria-describedby="basic-addon1"
+                      type="text"
+                      placeholder="npub..."
+                      value={pubkey()}
+                      onInput={(e) => setPubkey(e.currentTarget.value)}
+                    />
+                  </InputGroup>
 
-          <Form.Group class="mb-3" controlId="relayURL">
-            <Form.Label>relayURL</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="wss://"
-              value={relayURL()}
-              onInput={(e) => setRelayURL(e.currentTarget.value)}
-            />
-          </Form.Group>
+                  <Form.Group class="mb-3" controlId="relayURL">
+                    <Form.Label>relayURL</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="wss://"
+                      value={relayURL()}
+                      onInput={(e) => setRelayURL(e.currentTarget.value)}
+                    />
+                  </Form.Group>
 
-          <Button variant="primary" type="button" onClick={connectRelay}>
-            Submit
-          </Button>
-        </Form>
-        <hr />
-        {event() !== null && (
+                  <Button
+                    variant="primary"
+                    type="button"
+                    onClick={connectRelay}
+                  >
+                    取得
+                  </Button>
+                </Form>
+
+                <Show when={event() !== null}>
+                  <>
+                    <hr />
+                    <h3 class="fs-3">Event</h3>
+                    <pre>{JSON.stringify(event(), null, 2)}</pre>
+                  </>
+                </Show>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </>
+        <Show when={Object.keys(content()).length > 0}>
           <>
-            <h3 class="fs-3">Event</h3>
-            <pre>{JSON.stringify(event(), null, 2)}</pre>
-          </>
-        )}
-        {Object.keys(content()).length > 0 && (
-          <>
-            <h3 class="fs-3">profileを修正する</h3>
             <Form>
-              {Object.keys(content()).map((key) => (
-                <div class={styles.content}>
-                  <Row>
-                    <Form.Label column lg={2}>
-                      {key}
-                    </Form.Label>
-                    <Col>
-                      <InputGroup>
-                        <FormControl
-                          as="textarea"
-                          placeholder={key}
-                          type="text"
-                          value={content()[key]}
-                          readOnly={editingKey() !== key}
-                          onChange={(e) => {
-                            const updatedContent = {
-                              ...content(),
-                              [key]:
-                                e.target.value === "true"
-                                  ? true
-                                  : e.target.value === "false"
-                                  ? false
-                                  : e.target.value,
-                            };
-                            setContent(updatedContent);
-                          }}
-                        />
-                        {editingKey() !== key && (
-                          <>
-                            <Button
-                              variant="outline-primary"
-                              onClick={() => handleEdit(key)}
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              variant="outline-primary"
-                              onClick={() => handleDelete(key)}
-                            >
-                              Delete
-                            </Button>
-                          </>
-                        )}
-                        {editingKey() === key && (
-                          <Button
-                            variant="outline-primary"
-                            onClick={() => handleSave()}
+              <For each={Object.keys({ ...sampleData, ...content() })}>
+                {(key) => (
+                  <div class={styles.content}>
+                    <Row>
+                      <Form.Label column lg={2}>
+                        {key}
+                      </Form.Label>
+                      <Col>
+                        <InputGroup>
+                          <FormControl
+                            as="textarea"
+                            placeholder={
+                              sampleData.hasOwnProperty(key)
+                                ? sampleData[key]
+                                : key
+                            }
+                            type="text"
+                            value={content()[key]}
+                            readOnly={editingKey() !== key}
+                            onChange={(e) => {
+                              const updatedContent = {
+                                ...content(),
+                                [key]:
+                                  e.target.value === "true"
+                                    ? true
+                                    : e.target.value === "false"
+                                    ? false
+                                    : e.target.value,
+                              };
+                              setContent(updatedContent);
+                            }}
+                          />
+                          <Show
+                            when={editingKey() !== key}
+                            fallback={
+                              <Button
+                                variant="outline-primary"
+                                onClick={() => handleSave()}
+                              >
+                                Save
+                              </Button>
+                            }
                           >
-                            Save
-                          </Button>
-                        )}
-                      </InputGroup>
-                    </Col>
-                  </Row>
-                </div>
-              ))}
-
+                            <>
+                              <Button
+                                variant="outline-primary"
+                                onClick={() => handleEdit(key)}
+                              >
+                                Edit
+                              </Button>
+                              <Button
+                                variant="outline-primary"
+                                onClick={() => handleDelete(key)}
+                              >
+                                Delete
+                              </Button>
+                            </>
+                          </Show>
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+              </For>
               <div>
                 <input
                   type="text"
@@ -426,20 +481,31 @@ const App: Component = () => {
               </Button>
             </InputGroup>
           </>
-        )}
+        </Show>
 
-        {newEvent() !== null && (
+        <Show when={newEvent() !== null}>
           <>
             <hr />
-            <h3 class="fs-3">修正済Event</h3>
+            <h3 class="fs-3">Event</h3>
             <pre>{JSON.stringify(newEvent(), null, 2)}</pre>
             <hr />
             <h3 class="fs-3">Relayに投稿</h3>
-            <Button variant="warning" onClick={() => handlePublieshEvent()}>
-              投稿
-            </Button>
+            <Form>
+              <Form.Group class="mb-3" controlId="relayURL">
+                <Form.Label>relayURL</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="wss://"
+                  value={relayURL()}
+                  onInput={(e) => setRelayURL(e.currentTarget.value)}
+                />
+              </Form.Group>
+              <Button variant="warning" onClick={() => handlePublieshEvent()}>
+                投稿
+              </Button>
+            </Form>
           </>
-        )}
+        </Show>
       </Container>
 
       <ToastContainer class="p-3 position-fixed" position={"bottom-center"}>
