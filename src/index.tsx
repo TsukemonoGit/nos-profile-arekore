b5c340c824fbfbb7147ed57e5d4ab6a931c4d3cd
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-04-09 21:30:47 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -13,3 +13,4 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
     "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
+render(() => <App />, root!);
